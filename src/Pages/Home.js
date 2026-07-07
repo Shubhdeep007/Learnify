b0bfc678bdbe2c4a42e5b5750e87e1a256b95cae
Shubhdeep007 <shubhdeepmandhania@gmail.com>
2026-07-08 00:52:34 +0530

    Initial backend setup

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+
+
+function Home()=>{
+    return (
+        <div>
+            {/* section 1 */}
+
+            {/* section 2 */}
+            {/* section 3 */}
+            {/* section 4 */}
+        </div>
+    )
+}
